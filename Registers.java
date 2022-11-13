8342e21e1c027297e8edb3b5c18d7d6c838d6cd8
Pavan Reddy <86465783+pavanreddy-ml@users.noreply.github.com>
2022-11-13 06:21:15 -0500

    Update Registers.java

--- Registers.java
+++ Registers.java
@@ -1,31 +1,38 @@
 public class Registers
 {
-    public static int[] GPRS = {0, 0, 0, 0};
-    public static int[] IXRS = {0, 0, 0};
-    public static int IR = 0;
-    public static int MAR = 0;
-    public static int MBR = 0;
-    public static int MFR = 0;
-    public static int PC = 0;
-    public static boolean CC1 = false;
-    public static boolean CC2 = false;
-    public static boolean CC3 = false;
-    public static boolean CC4 = false;
+    private final SystemControl systemControl;
 
-
-    public static void reset_registers()
+    public Registers(SystemControl systemControl)
     {
-        Registers.GPRS = new int[]{0, 0, 0, 0};
-        Registers.IXRS = new int[]{0, 0, 0};
-        Registers.IR = 0;
-        Registers.MAR = 0;
-        Registers.MBR = 0;
-        Registers.MFR = 0;
-        Registers.PC = 0;
-        Registers.CC1 = false;
-        Registers.CC2 = false;
-        Registers.CC3 = false;
-        Registers.CC4 = false;
+        this.systemControl = systemControl;
+    }
+
+    public int[] GPRS = {0, 0, 0, 0};
+    public int[] IXRS = {0, 0, 0};
+    public int IR = 0;
+    public int MAR = 0;
+    public int MBR = 0;
+    public int MFR = 0;
+    public int PC = 0;
+    public boolean CC1 = false;
+    public boolean CC2 = false;
+    public boolean CC3 = false;
+    public boolean CC4 = false;
+
+
+    public void reset_registers()
+    {
+        GPRS = new int[]{0, 0, 0, 0};
+        IXRS = new int[]{0, 0, 0};
+        IR = 0;
+        MAR = 0;
+        MBR = 0;
+        MFR = 0;
+        PC = 0;
+        CC1 = false;
+        CC2 = false;
+        CC3 = false;
+        CC4 = false;
     }
 
 }
