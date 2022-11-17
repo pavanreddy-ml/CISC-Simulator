d2b557252ad157b54e48aeabab04298b98d5a01f
Pavan Reddy <86465783+pavanreddy-ml@users.noreply.github.com>
2022-11-17 17:27:20 -0500

    Add files via upload

---
+++ src/Memory.java
@@ -0,0 +1,93 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Memory
+{
+    private final SystemControl systemControl;
+
+    public Memory(SystemControl systemControl)
+    {
+        this.systemControl = systemControl;
+        int size = 2048;
+    }
+
+    int size = 2048;
+    int[] ProcessorMemory = new int[size];
+    int[] ProcessorMemoryTemp;
+
+    public void create_memory(int mem_size)
+    {
+        size = mem_size;
+        ProcessorMemory = new int[size];
+        Arrays.fill(ProcessorMemory, 0);
+    }
+
+    public void reset_memory()
+    {
+        ProcessorMemory = new int[size];
+        Arrays.fill(ProcessorMemory, 0);
+    }
+
+    public void modify_memory(String modification)
+    {
+        if (modification.equals("expand") && ProcessorMemory.length == 2048)
+        {
+            ProcessorMemoryTemp = new int[4096];
+            for(int i=0; i<ProcessorMemory.length; i++)
+            {
+                ProcessorMemoryTemp[i] = ProcessorMemory[i];
+            }
+            ProcessorMemory = ProcessorMemoryTemp;
+            size = 4096;
+        }
+        else if (modification.equals("contract") && ProcessorMemory.length == 4096)
+        {
+            ProcessorMemoryTemp = new int[2048];
+            for(int i=0; i<ProcessorMemoryTemp.length; i++)
+            {
+                ProcessorMemoryTemp[i] = ProcessorMemory[i];
+            }
+            ProcessorMemory = ProcessorMemoryTemp;
+            size = 2048;
+        }
+    }
+
+    public int get_from_memory(int addr)
+    {
+        systemControl.registers.MAR = addr;
+
+        systemControl.registers.MBR = systemControl.cache.read(systemControl.registers.MAR);
+
+        if (systemControl.registers.MBR == -1) {
+            systemControl.PrintToDebugConsole(String.format("Cache missed at %H", systemControl.registers.MAR));
+            systemControl.registers.MBR = ProcessorMemory[systemControl.registers.MAR];
+            systemControl.simulateDelay();
+            systemControl.cache.write(systemControl.registers.MAR, systemControl.registers.MBR);
+        }
+
+        return systemControl.registers.MBR;
+    }
+
+    public void store_to_memory(int addr, int val)
+    {
+        systemControl.registers.MAR = addr;
+
+        systemControl.registers.MBR = val;
+
+        systemControl.cache.write(systemControl.registers.MAR, systemControl.registers.MBR);
+
+        systemControl.PrintToDebugConsole(String.format("Write %d to %H", val, addr));
+
+        ProcessorMemory[addr] = val;
+
+        systemControl.frame.update_memory_single(val, addr);
+    }
+
+    public void clear_memory_address(int addr)
+    {
+        ProcessorMemory[addr] = 0;
+
+        systemControl.frame.update_memory_single(0, addr);
+    }
+
+}
---
+++ src/Registers.java
@@ -0,0 +1,48 @@
+public class Registers
+{
+    private final SystemControl systemControl;
+
+    public Registers(SystemControl systemControl)
+    {
+        this.systemControl = systemControl;
+    }
+
+    public int[] GPRS = {0, 0, 0, 0};
+    public int[] IXRS = {0, 0, 0};
+    public int IR = 0;
+    public int MAR = 0;
+    public int MBR = 0;
+    public int PC = 0;
+
+    public boolean CC1 = false;
+    public boolean CC2 = false;
+    public boolean CC3 = false;
+    public boolean CC4 = false;
+
+    public boolean MFR1 = false;
+    public boolean MFR2 = false;
+    public boolean MFR3 = false;
+    public boolean MFR4 = false;
+
+
+    public void reset_registers()
+    {
+        GPRS = new int[]{0, 0, 0, 0};
+        IXRS = new int[]{0, 0, 0};
+        IR = 0;
+        MAR = 0;
+        MBR = 0;
+        PC = 0;
+
+        CC1 = false;
+        CC2 = false;
+        CC3 = false;
+        CC4 = false;
+
+        MFR1 = false;
+        MFR2 = false;
+        MFR3 = false;
+        MFR4 = false;
+    }
+
+}
